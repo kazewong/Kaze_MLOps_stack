2d14bcf5fc914bdc827521da30b283209fba2f95
Kaze Wong <kazewong.physics@gmail.com>
2025-04-13 21:35:17 -0400

    Finish transferring model code

--- src/kazemlstack/cli/train_vit_cifar.py
+++ src/kazemlstack/cli/train_vit_cifar.py
@@ -1,14 +1,33 @@
 import fire
 import jax
 import jax.numpy as jnp
+import flax.nnx as nnx
 import optax
 
 from kazemlstack.model.ViT import VisionTransformer
 
-def train_step(
-        
-):
+
+def train_step():
     pass
+
+def test_model():
+    model = VisionTransformer(
+        rng_seed=0,
+        embed_dim=16,
+        hidden_dim=64,
+        num_heads=4,
+        num_channels=3,
+        num_layers=4,
+        patch_size=4,
+        num_patches=64,
+    )
+
+    test_input = jnp.zeros((10, 3, 16, 16))
+    def single_eval(input):
+        return model(input)
+
+    test_output = nnx.jit(nnx.vmap(single_eval))(test_input)
+    print(test_output.shape)  # Should be (10, 64, 16)
 
 
 
@@ -18,7 +37,17 @@ def train(
     learning_rate: float = 0.001,
     momentum: float = 0.9,
 ):
-    print(1)    
+    model = VisionTransformer(
+        rng_seed=0,
+        embed_dim=16,
+        hidden_dim=64,
+        num_heads=4,
+        num_channels=3,
+        num_layers=4,
+        patch_size=4,
+        num_patches=64,
+    )
 
-if __name__ == '__main__':
-   fire.Fire(train)
+
+if __name__ == "__main__":
+    fire.Fire(test_model)
